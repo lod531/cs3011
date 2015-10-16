2f5d6dd8407f341d4ccd2cf5063e8399e9685a35
Andy <buinovsa@tcd.ie>
2015-10-16 15:32:02 -0700

    Exercises 1 & 2 done.

--- homework/homework1.pl
+++ homework/homework1.pl
@@ -1,8 +1,37 @@
-numeral(0).
-numeral(s(X)):- numeral(X).
-numeral(X+Y):- numeral(X), numeral(Y).
+%cs3011 homework #1, Student Number: 13319368
+%Prelude:
+
+
 
 add(0, X, X).
 add(s(X), Y, s(Z)):- add(X, Y, Z).
+add(p(X), Y, p(Z)):- add(X, Y, Z).
 
-%Exercise 1
+numeral(0).
+numeral(X+Y):- numeral(X), numeral(Y).
+numeral(s(X)):- numeral(X).
+numeral(p(X)):- numeral(X).
+
+
+%Exercise 1 & 2
+
+
+
+add2(X0+X1, Y0+Y1, Z)	:- add(X0, X1, Xs), add(Y0, Y1, Ys), add(Xs, Ys, Zs), simplify(Zs, Z).
+add2(X0+X1, Y, Z)		:- add(X0, X1, Xs), add(Xs, Y, Z).
+add2(X, Y0+Y1, Z)		:- add(Y0, Y1, Ys), add(X, Ys, Z).
+add2(X, Y, Z)			:- add(X, Y, Z).
+
+
+
+simplify(W, Z)						:- simplifyHelper(W, 0, 0, Z).
+
+simplifyHelper(0, 0, 0, Z)			:- numeral(Z).
+simplifyHelper(s(W), X, Y, Z)		:- simplifyHelper(W, s(X), Y, Z).
+simplifyHelper(p(W), X, Y, Z)		:- simplifyHelper(W, X, p(Y), Z).
+simplifyHelper(0, s(X), p(Y), Z)	:- simplifyHelper(0, X, Y, Z).
+simplifyHelper(0, s(X), 0, s(Z))	:- simplifyHelper(0, X, 0, Z).
+simplifyHelper(0, 0, p(Y), p(Z))	:- simplifyHelper(0, 0, Y, Z).
+
+%Exercise 3
+
