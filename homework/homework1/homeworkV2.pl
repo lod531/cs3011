9d7f9c1e2529c10d1aa1367766d387229737a0a3
Andy <buinovsa@tcd.ie>
2015-10-20 11:07:30 -0700

    Homework complete!

--- homework/homework1/homeworkV2.pl
+++ homework/homework1/homeworkV2.pl
@@ -7,16 +7,18 @@ numeral(X-Y):- numeral(X), numeral(Y).
 numeral(s(X)):- numeral(X).
 numeral(p(X)):- numeral(X).
 
-add(0, s(Y), Z)			:- add(Y, Y, Ys), simplify(Zs, Z).
-add(s(X), Y, s(Z))		:- add(X, Y, Z).
-add(p(X), Y, p(Z))		:- add(X, Y, Z).
-
 %Exercise 1 
 
 
-
+add2(0, 0, 0).
 add2(0, Y, Z)			:- add2(Y, 0, Zs), simplify(Zs, Z).
-add2()
+add2(s(X), Y, s(Z)) 		:- add2(X, Y, Zs), simplify(Zs, Z).
+add2(p(X), Y, p(Z))		:- add2(X, Y, Zs), simplify(Zs, Z).	
+add2(X0+X1, Y, Z)		:- add2(X0, X1, Xs), add2(Y, 0, Ys), 
+					add2(Xs, Ys, Zs), simplify(Zs, Z).
+add2(-X, Y, Z)			:- add2(X, 0, Xs), minus(Xs, Xss), add2(Xss, Y, Zs), simplify(Zs, Z).
+add2(X0-X1, Y, Z)		:- subtract(X0, X1, Xs), add2(Y, 0, Ys),
+					add2(Xs, Ys, Zs), simplify(Zs, Z).
 %Exercise 2 helper declarations
 
 
@@ -31,8 +33,13 @@ simplifyHelper(0, 0, p(Y), p(Z))	:- simplifyHelper(0, 0, Y, Z).
 
 %Exercise 3
 
-minus(X, Y)							:-simplify(X, Z), minusHelper(Z, Y).
+minus(X, Y)					:-simplify(X, Z), minusHelper(Z, Y).
 
-minusHelper(0, Y)					:- numeral(Y).
+minusHelper(0, Y)				:- numeral(Y).
 minusHelper(s(X), p(Y))				:- minusHelper(X, Y).
 minusHelper(p(X), s(Y))				:- minusHelper(X, Y).
+
+%Exercise 5
+
+subtract(X, Y, Z)				:- add2(X, 0, Xs), add2(Y, 0, Ys), minus(Ys, Yss),
+							add2(Xs, Yss, Zs), simplify(Zs, Z).	
